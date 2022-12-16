cc1c4a5cb9a9db9c6d247ea59e6a6c5b72718d19
valentin <valentinchailloux7@gmail.com>
2022-12-16 11:55:47 +0100

    Git innit FicheFrais User (1 par 1)

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-public function index(AuthenticationUtils $authenticationUtils): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -19,7 +19,23 @@ public function index(AuthenticationUtils $authenticationUtils): Response
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
             'error' =>  $error,
+
             ]);
+
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+        return new Response('Well hi there '.$user->getFirstName());
+
     }
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout()
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
+
+
+
 }
 
